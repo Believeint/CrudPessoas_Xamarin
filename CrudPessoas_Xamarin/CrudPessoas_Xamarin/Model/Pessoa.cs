806b32cbe8e6786db9c90509e52df43c753ae30f
Elias Carvalho <Elias.carvalho.96@live.com>
2019-10-11 11:43:15 -0300

    Plugin Sqlite, Classe Pessoa

---
+++ CrudPessoas_Xamarin/CrudPessoas_Xamarin/Model/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace CrudPessoas_Xamarin.Model
+{
+    public class Pessoa
+    {
+        // Propriedades classe Pessoa
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Pais { get; set; }
+    }
+}
